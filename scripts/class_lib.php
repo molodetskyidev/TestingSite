10eb243ddb4b1e861ba415ea652ef198960edbaa
Oleksandr Molodetskyi <molodetskyidev@gmail.com>
2019-01-02 19:17:15 +0200

    created basic login functionality

--- scripts/class_lib.php
+++ scripts/class_lib.php
@@ -1,34 +1,63 @@
 <?php 
-class db { //start of class
+class db { // start of class
    var $servername = "localhost";
    var $username ="root" ;
    var $password = "r2d2";
+   var $dbname = "loginregister";
    
-   function init(){ //start of init function
+function init(){ //start of init function
      // Create connection
-   $conn = new mysqli("localhost", "root", "r2d2");
+   $conn = new mysqli("localhost", "root", "r2d2","loginregister");
 
     // Check connection
     if ($conn->connect_error) { 
-      die("Connection failed: " . $conn->connect_error);
+      die("Connection with DB failed: " . $conn->connect_error."</br>");
      }  else {
-     echo "Connected successfully";
+     //TODO investigate how to show in log instead of the page
+    // echo "Connected successfully</br>";
      return $conn;
    }
    } //end of init function
    
-   function login($username, $password){
-   $sql = "SELECT * FROM USERS WHERE username=".$username." AND password=".$password;
-if ($conn->query($sql) === TRUE) {
-    echo "query was successful";
+function login($username, $password,$conn){
+   $sql = "SELECT * FROM USERS WHERE username='".$username."' AND password='".$password."';";
+   $result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0) {
+ echo "<h2>User ".$username." is logged!</h2>";
+ echo "<h3>Details:</h3>";
+ while($row = mysqli_fetch_assoc($result)) {
+  echo "id: " . $row["user_id"]. "</br>username: " . $row["username"]. "</br>name: " . $row["name"]. "</br>surname: " . $row["surname"]. "</br>sex: ". $row["sex"]. "</br>date of birth: ". $row["date_of_birth"]."</br>date of registration: ". $row["date_of_registration"]."</br>email: ". $row["email"]."</br>picture: ". $row["picture"]."<br>";
+  }
+  }
+  else {
+    echo "<h2>user or password is incorrect.</h2> Try to <a href='../html/login.html'>login</a> again with correct data or <a href='../html/register.html'>register!</a></h2>";
+}
+echo "</br>";
+}
+
+function takeAllUsers($conn){
+$sql = "SELECT * FROM USERS;";
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0) {
+   echo "<h3>Details:</h3>";
+ while($row = mysqli_fetch_assoc($result)) {
+  echo "id: " . $row["user_id"]. "</br>username: " . $row["username"]. "</br>name: " . $row["name"]. "</br>surname: " . $row["surname"]. "</br>sex: ". $row["sex"]. "</br>date of birth: ". $row["date_of_birth"]."</br>date of registration: ". $row["date_of_registration"]."</br>email: ". $row["email"]."</br>picture: ". $row["picture"]."<br>";
+    }
 } else {
-    echo "error while doing the query: " . $conn->error;
+    echo "0 results";
+}
+
+}
+
+function closeConnection($conn){
+$result=mysqli_close($conn); 
+if($result) {
+//TODO investigate how to show in log instead of the page
+// echo "Connection closed</br>";
+} else {
+echo "There is an issue with closing connection</br>";
 }
 }
-function closedb($test);
-   {
-   echo "Connection closed"
-   }
-  
-   } //end of class
+
+} // end of class
 ?>
--- scripts/login.php
+++ scripts/login.php
@@ -1,7 +1,18 @@
 <?php
 include("class_lib.php"); 
-echo "DB test<br/>";
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+ $username=check_data($_POST["login"]);
+ $password=check_data($_POST["password"]);
  $db=new db();
  $conn=$db->init();
-
+ $db->login($username,$password,$conn);
+ echo "</br><a href='../html/login.html'>Back to Login page</a>";
+ $db->closeConnection($conn);
+function check_data($data){
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
 ?>
